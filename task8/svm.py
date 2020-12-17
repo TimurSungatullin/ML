5d030bd235cc80964a7d1e4e4aa197cee2d507cb
timursungatullin <sungatullin@technokratos.com>
2020-12-17 14:41:01 +0300

    Fix svm

--- task8/svm.py
+++ task8/svm.py
@@ -8,7 +8,8 @@ BLUE = pg.Color('blue')
 LEFT = 1
 RIGHT = 3
 
-sc = pg.display.set_mode((600, 400))
+width, height = 600, 400
+sc = pg.display.set_mode((width, height))
 sc.fill(WHITE)
 pg.display.update()
 
@@ -34,6 +35,7 @@ while is_exit:
         if i.type == pg.QUIT:
             is_exit = False
         elif i.type == pg.MOUSEBUTTONDOWN and i.button in (LEFT, RIGHT):
+            # Правая кнопка рисует синюю, а левая - красную
             # Проставление точек
             if i.button == LEFT:
                 color = RED
@@ -50,6 +52,7 @@ while is_exit:
             pg.display.update()
 
         elif i.type == pg.KEYDOWN and i.key == pg.K_RETURN:
+            # Enter чтобы нарисовать линии
             # Разделение точек
             redraw_dots(dots)
             clf = svm.SVC(kernel='linear', C=1.0)
@@ -72,10 +75,28 @@ while is_exit:
                 -reverse_coefficient[1] + Y_1,
                 -reverse_coefficient[0] + X_1
             )
-            pg.draw.line(sc, GREEN, [0, Y_1], [X_1, 0], 1)
-            pg.draw.aaline(sc, GREEN, [0, Y_2], [X_2, 0])
-            pg.draw.aaline(sc, GREEN, [0, Y_3], [X_3, 0])
+            print(X_1, Y_1, X_2, Y_2, X_3, Y_3)
+            # Коэффициенты прямой
+            b1 = Y_1
+            b2 = Y_2
+            b3 = Y_3
+            k = - Y_1 / X_1
+            if k < 0:
+                end = 0
+            else:
+                end = height
 
+            pg.draw.line(sc, GREEN, [0, Y_1], [(end - b1) / k, end], 1)
+            pg.draw.aaline(sc, GREEN, [0, Y_2], [(end - b2) / k, end])
+            pg.draw.aaline(sc, GREEN, [0, Y_3], [(end - b3) / k, end])
+
+            pg.display.update()
+
+        elif i.type == pg.KEYDOWN and i.key == pg.K_SPACE:
+            # Пробел, чтобы полностью очистить поле
+            sc.fill(WHITE)
+            dots = {}
+            colors = []
             pg.display.update()
 
     pg.time.delay(30)
